17bc181250ca9a3f1591a3556aa5ed71d41e0c44
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 09:51:24 +0300

    create custom_status_code view, includes:
    - custom status code
    - process form data
    - process json data
    - show raw bytes data

--- blog/app.py
+++ blog/app.py
@@ -11,6 +11,8 @@ def index():
 @app.route("/greet/<name>/")
 def greet_name(name: str):
     """
+    # https://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+
     Visit http://127.0.0.1:5000/greet/GitHub/
 
     :param name:
@@ -33,3 +35,52 @@ def read_user():
     name = request.args.get("name")
     surname = request.args.get("surname")
     return f"User {name or '[no name]'} {surname or '[no surname]'}"
+
+
+@app.route("/status/", methods=["GET", "POST"])
+def custom_status_code():
+    """
+    # https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+
+    # GET
+    >  curl --request GET --url http://127.0.0.1:5000/status/
+    - To get response with custom status code
+      send request using POST method
+      and pass `code` in JSON body / FormData
+
+    # POST (empty body)
+    > curl --request POST --url http://127.0.0.1:5000/status/
+    < HTTP/1.0 204 NO CONTENT
+
+    # POST (multipart/form-data)
+    > curl --request POST --url http://127.0.0.1:5000/status/ \
+      --header 'Content-Type: multipart/form-data' \
+      --form code=202
+    < HTTP/1.0 202
+    - code from form
+
+    # POST (json)
+    > curl --request POST --url http://127.0.0.1:5000/status/ \
+      --header 'Content-Type: application/json' \
+      --data '{"code": 205}'
+    < HTTP/1.0 205 RESET CONTENT
+    - code from json
+
+    :return:
+    """
+    if request.method == "GET":
+        return """\
+        To get response with custom status code 
+        send request using POST method 
+        and pass `code` in JSON body / FormData
+        """
+
+    print("raw bytes data:", request.data)
+
+    if request.form and "code" in request.form:
+        return "code from form", request.form["code"]
+
+    if request.json and "code" in request.json:
+        return "code from json", request.json["code"]
+
+    return "", 204
