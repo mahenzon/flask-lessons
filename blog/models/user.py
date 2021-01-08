9463f568713b8e55721eae918da2206bc052aa76
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 15:44:19 +0300

    add UserMixin to User model

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,9 +1,10 @@
+from flask_login import UserMixin
 from sqlalchemy import Column, Integer, String, Boolean
 
 from blog.models.database import db
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = Column(Integer, primary_key=True)
     username = Column(String(80), unique=True, nullable=False)
     is_staff = Column(Boolean, nullable=False, default=False)
