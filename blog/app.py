ca62da332af234e1740deff3a82435f1ab826b58
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 10:18:04 +0300

    show errorhandler and app.logger.exception

--- blog/app.py
+++ blog/app.py
@@ -148,3 +148,38 @@ def power_value():
     result = x ** y
     app.logger.debug("%s ** %s = %s", x, y, result)
     return str(result)
+
+
+@app.route("/divide-by-zero/")
+def do_zero_division():
+    """
+    # http://127.0.0.1:5000/divide-by-zero/
+    < HTTP/1.0 400 BAD REQUEST
+    - Never divide by zero!
+
+    :return:
+    """
+    return 1 / 0
+
+
+@app.errorhandler(ZeroDivisionError)
+def handle_zero_division_error(error):
+    """
+    Process unhandled zero division error
+
+    [log]: ERROR in app: Here's traceback for zero division error
+        Traceback (most recent call last):
+          File "/usr/lib/python3.9/site-packages/flask/app.py", line 1950, in full_dispatch_request
+            rv = self.dispatch_request()
+          File "/usr/lib/python3.9/site-packages/flask/app.py", line 1936, in dispatch_request
+            return self.view_functions[rule.endpoint](**req.view_args)
+          File "/apps/flask-lesson/blog/app.py", line 155, in do_zero_division
+            return 1 / 0
+        ZeroDivisionError: division by zero
+
+    :param error:
+    :return:
+    """
+    print(error)  # prints str version of error: 'division by zero'
+    app.logger.exception("Here's traceback for zero division error")
+    return "Never divide by zero!", 400
