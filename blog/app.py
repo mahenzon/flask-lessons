25753fb794f9fc523ffed796ab481c698c9e5553
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 14:33:39 +0300

    initialize Flask-SQLAlchemy

--- blog/app.py
+++ blog/app.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 from blog.views.users import users_app
 from blog.views.articles import articles_app
+from blog.models.database import db
 
 app = Flask(__name__)
 
 app.register_blueprint(users_app, url_prefix="/users")
 app.register_blueprint(articles_app, url_prefix="/articles")
+
+# db
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/blog.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 
 @app.route("/")
