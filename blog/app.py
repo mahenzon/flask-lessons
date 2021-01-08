3c9904e99bfdb8df7051bcd7e113446bd96fc137
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 09:22:00 +0300

    path parsing: create greet_name view

--- blog/app.py
+++ blog/app.py
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello web!"
+
+
+@app.route("/greet/<name>/")
+def greet_name(name: str):
+    """
+    Visit http://127.0.0.1:5000/greet/GitHub/
+
+    :param name:
+    :return:
+    """
+    return f"Hello {name}!"
