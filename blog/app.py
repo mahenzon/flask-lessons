92da299e27f1c702abf989a7b115c6f0cd63ee3f
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-08 13:09:20 +0300

    create articles blueprint, list view; add to navbar

--- blog/app.py
+++ blog/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
 from blog.views.users import users_app
+from blog.views.articles import articles_app
 
 app = Flask(__name__)
 
 app.register_blueprint(users_app, url_prefix="/users")
+app.register_blueprint(articles_app, url_prefix="/articles")
 
 
 @app.route("/")
