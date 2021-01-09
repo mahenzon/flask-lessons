38c9984347d8eea5bccb06a44d4c4fcba4eb5669
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-09 12:47:19 +0300

    modify command create-users to create-admin

--- blog/app.py
+++ blog/app.py
@@ -31,29 +31,18 @@ def index():
     return render_template("index.html")
 
 
-@app.cli.command("init-db")
-def init_db():
+@app.cli.command("create-admin")
+def create_admin():
     """
     Run in your terminal:
-    flask init-db
-    """
-    db.create_all()
-    print("done!")
-
-
-@app.cli.command("create-users")
-def create_users():
-    """
-    Run in your terminal:
-    flask create-users
-    > done! created users: <User #1 'admin'> <User #2 'james'>
+    ➜ flask create-admin
+    > created admin: <User #1 'admin'>
     """
     from blog.models import User
+
     admin = User(username="admin", is_staff=True)
-    james = User(username="james")
 
     db.session.add(admin)
-    db.session.add(james)
     db.session.commit()
 
-    print("done! created users:", admin, james)
+    print("created admin:", admin)
