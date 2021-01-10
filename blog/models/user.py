73db2408f9d47d4d2855d6e0ff85418294a84115
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-10 12:11:22 +0300

    create Author model, add relationship to user, migrate

--- blog/models/user.py
+++ blog/models/user.py
@@ -1,5 +1,6 @@
 from flask_login import UserMixin
 from sqlalchemy import Column, Integer, String, Boolean, LargeBinary
+from sqlalchemy.orm import relationship
 
 from blog.security import flask_bcrypt
 from blog.models.database import db
@@ -13,6 +14,8 @@ class User(db.Model, UserMixin):
     is_staff = Column(Boolean, nullable=False, default=False)
     email = Column(String(255), unique=True, nullable=False, default="", server_default="")
     _password = Column(LargeBinary, nullable=True)
+
+    author = relationship("Author", uselist=False, back_populates="user")
 
     @property
     def password(self):
