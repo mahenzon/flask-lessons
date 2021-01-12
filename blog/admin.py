6f159002a9488a9d4f66ed117f2c1c5c84c3f706
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-12 11:35:15 +0300

    view access limit example

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,7 @@
+from flask import redirect, url_for
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
 
 from blog import models
 from blog.models.database import db
@@ -7,7 +9,13 @@ from blog.models.database import db
 
 # Customized admin interface
 class CustomView(ModelView):
-    pass
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.is_staff
+
+    def inaccessible_callback(self, name, **kwargs):
+        # redirect to login page if user doesn't have access
+        return redirect(url_for("auth_app.login"))
 
 
 class TagAdminView(CustomView):
