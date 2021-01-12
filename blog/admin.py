8f5e24b1f26ccdcbe167fb2986bbf0605ebe1b32
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-01-12 11:04:36 +0300

    create TagAdminView

--- blog/admin.py
+++ blog/admin.py
@@ -10,9 +10,16 @@ class CustomView(ModelView):
     pass
 
 
+class TagAdminView(CustomView):
+    column_searchable_list = ("name",)
+    column_filters = ("name",)
+    can_export = True
+    export_types = ["csv", "xlsx"]
+
+
 # Create admin with custom base template
 admin = Admin(name="Blog Admin", template_mode="bootstrap4")
 
 # Add views
-admin.add_view(CustomView(models.Tag, db.session, category="Models"))
+admin.add_view(TagAdminView(models.Tag, db.session, category="Models"))
 admin.add_view(CustomView(models.Article, db.session, category="Models"))
